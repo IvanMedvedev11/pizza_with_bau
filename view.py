0cc5ddb75e5b1e1b8cde23484f8ad3512880d587
IvanMedvedev11 <medvivan11@gmail.com>
2024-11-30 18:58:14 +0700

    Update view.py

--- view.py
+++ view.py
@@ -1,3 +1,4 @@
+import json
 def register():
     name = input("Введите имя: ")
     surname = input("Ввежите фамилию: ")
@@ -6,12 +7,13 @@ def register():
     return name, surname, age, number
 
 
-def custom_pizza(products):
-
+def custom_pizza():
     size = input("Выберите размер: 38; 40; 45: ")
     user_dopings = {}
-    for product in products:
-        print(product['name'])
+    with open('products.json', 'r', encoding='UTF-8') as file:
+        products = json.load(file)
+        for product in products:
+            print(product['name'])
     while True:
         user_doping = input("Что вы хотите добавить(или 'стоп'): ")
         if user_doping == 'стоп':
@@ -19,17 +21,22 @@ def custom_pizza(products):
         else:
             user_count_doping = input(f"Сколько {user_doping} вы хотите взять: ")
             user_dopings[user_doping] = user_count_doping
-
-
 def choose_pizza():
-    user_pizza = input("Выберите пиццу: ")
-    if user_pizza == "Кастомная пицца":
-        return user_pizza, '0'
-    user_count = input(f"Сколько {user_pizza} вы хотите взять: ")
-    return user_pizza, user_count
-
+    user_pizzas = {}
+    while True:
+        user_pizza = input("Выберите пиццу('стоп' или 'Кастомная пицца'): ")
+        if user_pizza == "стоп":
+            return user_pizzas
+        elif user_pizza == "Кастомная пицца":
+            user_pizzas[user_pizza] =  '1'
+        else:
+            user_count = input(f"Сколько {user_pizza} вы хотите взять: ")
+            user_pizzas[user_pizza] = user_count
 
 def type_of_pay():
     pay = input("Введите тип оплаты: ")
     summ = input("Внесите сумму: ")
     return pay, summ
+
+def print_check():
+    pass
