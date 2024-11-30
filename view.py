9fc165ece3d730174413a5376f74c37e191adc7b
IvanMedvedev11 <medvivan11@gmail.com>
2024-11-30 16:43:20 +0700

    Update view.py

--- view.py
+++ view.py
@@ -1,1 +1,19 @@
+def register():
+    name = input("Введите имя: ")
+    surname = input("Ввежите фамилию: ")
+    birth = input("Введите год рождения: ")
+    number = input("Введите номер телефона: ")
+    return name, surname, birth, number
+
+
+def custom_pizza():
+    size = input("Выберите размер: 38; 40; 45: ")
+    user_dopings = {}
+    while True:
+        user_doping = input("Что вы хотите добавить(или 'стоп'): ")
+        if user_doping == 'стоп':
+            return user_dopings
+        else:
+            user_count_doping = input(f"Сколько {user_doping} вы хотите взять: ")
+            user_dopings[user_doping] = user_count_doping
 
