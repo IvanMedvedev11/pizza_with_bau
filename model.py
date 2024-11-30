fe647132a8062040b1ecf7272905d5692cec3322
Anzam2000 <andrejzamaraev29@gmail.com>
2024-11-30 17:02:48 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,17 +1,10 @@
+
 def add_user(data,number,name,age,surname):
     if any(user["number"] == number for user in data):
         print("Уже есть такой пользователь с этим номером.")
     else:
         data.append({"name": name,"age": age, "surname": surname, "number": number})
         print("Пользователь успешно добавлен.")
-def search_user(data):
-    found = False
-    for person in data:
-        if person["name"].lower() == search_name.lower():
-            print(f"Данные о человеке с именем '{search_name}':")
-            print(
-                f"Имя: {person.get('name')}, Город: {person.get('town')}, Возраст: {person.get('age')}, Фамилия: {person.get('lastname')}, Номер: {person.get('number')}")
-            found = True
-            break
-    if not found:
-        print(f"Человек с именем '{search_name}' не найден.")
+def save_data(filename, data):
+    with open(filename, 'w') as file:
+        json.dump(data, file, indent=4)
